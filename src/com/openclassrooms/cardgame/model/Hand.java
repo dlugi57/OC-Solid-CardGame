17c893a68d0a395aefd098a5ced998451f01e877
dlugi57 <dlugoszpeter@gmail.com>
2020-07-22 13:09:21 +0200

    DEV: init model

---
+++ src/com/openclassrooms/cardgame/model/Hand.java
@@ -0,0 +1,19 @@
+package com.openclassrooms.cardgame.model;
+
+import java.util.ArrayList;
+
+public class Hand {
+    ArrayList<PlayingCard> cards;
+
+    public void addCard(PlayingCard pc) {
+        cards.add(pc);
+    }
+
+    public PlayingCard getCard(int index) {
+        return cards.get(index);
+    }
+
+    public PlayingCard removeCard() {
+        return cards.remove(0);
+    }
+}
